7be80a47bc4ce2d7a979e95c69dccf405489b847
batlukov <mence47@yandex.ru>
2019-07-04 13:59:19 +0300

    added simple functional to filter

--- person/src/main/java/servlets/MyFilter.java
+++ person/src/main/java/servlets/MyFilter.java
@@ -14,6 +14,7 @@ public class MyFilter implements Filter {
 
     public void doFilter(ServletRequest request, ServletResponse response,
     FilterChain chain) throws IOException, ServletException {
+        System.out.println("filtering request before result.jsp");
         chain.doFilter(request, response);
     }
 
